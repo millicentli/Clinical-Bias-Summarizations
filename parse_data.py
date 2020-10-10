11c9db63834644bb20ece118f490d31b0dadfd7d
Millicent Li <mllcntl@gmail.com>
2020-10-10 01:12:47 -0700

    added some code to streamline process, also updated filtering

--- parse_data.py
+++ parse_data.py
@@ -2,10 +2,16 @@
 
 import os
 import shutil
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--crawl', dest='crawl', action='store_true', help='crawls the original MIMIC-CXR dataset')
+parser.add_argument('--nocrawl', dest='crawl', action='store_false', help='do not crawl the original MIMIC-CXR dataset')
 
+parser.set_defaults(crawl=False)
+args = parser.parse_args()
 
-# Crawls through the directories, putting the files in the filtered directory
+# Crawls through the directories, filtering and putting the files in the filtered directory 
 def directory_crawl(src, dst, symlinks=False, ignore=None):
     for item in os.listdir(src):
         s = os.path.join(src, item)
@@ -13,19 +19,24 @@ def directory_crawl(src, dst, symlinks=False, ignore=None):
             directory_crawl(s, dst, symlinks, ignore)
         else:
             if 'txt' in s:
-                f = s.rfind("/")
-                d = os.path.join(dst, s[f + 1:])
-                if not os.path.exists(dst):
-                    os.makedirs(dst)
-                shutil.copy2(s, d)
+                with open(s, 'r') as f:
+                    if 'IMPRESSION' and 'FINDINGS' in f.read():
+                        f = s.rfind("/")
+                        d = os.path.join(dst, s[f + 1:])
+                        if not os.path.exists(dst):
+                            os.makedirs(dst)
+                        shutil.copy2(s, d)
 
 if __name__ == "__main__":
-    dst = "mimic-cxr/files/filtered/"
 
-    # p10
-    # directory_crawl("/data2/limill01/Clinical-Bias-Summarizations/mimic-cxr/files/p10", dst)
-    
-    for i in range(11, 20):
-        s = "p" + str(i)
-        print("directory:", s)
-        directory_crawl("/data2/limill01/Clinical-Bias-Summarizations/mimic-cxr/files/" + s, dst)
+    # start directory crawling for MIMIC-CXR, putting the text files in one folder
+    if args.crawl:
+        dst = "mimic-cxr/files/filtered/"
+
+        for i in range(10, 20):
+            s = "p" + str(i)
+            print("directory: " +  s)
+            directory_crawl("/data2/limill01/Clinical-Bias-Summarizations/mimic-cxr/files/" + s, dst)
+    else:
+        raise NotImplemented
+        
